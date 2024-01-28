777f5c49d2299823940f4469081450f4bdc715c5
Your Name <you@example.com>
2024-01-28 11:26:53 +0700

    commmit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "student";
+    return view("layout.master");
+});
+Route::prefix('/subject')->group(function () {
+    Route::get('/', [SubjectController::class, 'search']);
+    Route::get('/add', function () {
+        return view('subject.subject-add');
+    });
+    Route::post('/create', [SubjectController::class, 'create'])->name("subject.create");
+    Route::get('/edit/{id}', [SubjectController::class, 'find']);
+    Route::put('/update/{id}', [SubjectController::class, 'update'])->name('subject.update');
+    Route::get('/show/{id}', [SubjectController::class, 'show']);
+    Route::put('/delete/{id}', [SubjectController::class, 'delete'])->name('subject.delete');
+});
+Route::get('/student', function () {
+    return view('student.student-list');
 });
